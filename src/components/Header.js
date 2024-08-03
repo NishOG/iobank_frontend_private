e48bbd99f3d27588b7a91ad6ec1645741b3419de
iocods <Iocodes2000@gmail.com>
2024-08-03 13:58:55 +0100

    Added the functionality to enable the settings icon navigate to the settings page

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,13 @@
 import React from 'react'
 import { FaSmile, FaBars, FaExchangeAlt } from 'react-icons/fa'
 import { MdNotifications, MdSettings } from 'react-icons/md'
+import { useNavigate } from 'react-router-dom'
 
 const Header = () => {
+  const navigate = useNavigate()
+  const handleClick = (path) => {
+    navigate(path)
+  }
   return (
     <header className='fixed top-0 left-0 right-0 bg-white z-10 flex justify-between items-center p-3 lg:ml-250 pr-9 border-b-2'>
         <h1 className='flex items-center text-gray-600 text-lg font-bold gap-1 p-1'>Welcome Isaiah <FaSmile  size={20} color='#ffbf56'/> </h1>
@@ -15,7 +20,7 @@ const Header = () => {
                 </span>
                 <span className='text-sm'>Convert</span>
               </button>
-              <button className='text-blue-500 hover:text-blue-700 flex items-center text-xl transition duration-500 ease-in-out p-2 border rounded-full'>
+              <button onClick={() => handleClick('/dashboard/settings')} className='text-blue-500 hover:text-blue-700 flex items-center text-xl transition duration-500 ease-in-out p-2 border rounded-full'>
                 <MdSettings size={30}/>
               </button>
               <button className='text-blue-500 hover:text-blue-700 flex items-center text-xl transition duration-500 ease-in-out p-3 border rounded-full bg-gray-100'>
