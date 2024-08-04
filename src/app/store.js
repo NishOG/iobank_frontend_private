bec930df7fa2424b686995cc16d0f15997348c53
iocods <Iocodes2000@gmail.com>
2024-08-04 08:17:01 +0100

    Added the cardReducer to the store

--- src/app/store.js
+++ src/app/store.js
@@ -1,6 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
 import accountReducers from "../features/accounts/accountSlice";
 import pageReducers from "../features/page/pageSlice";
+import cardReducers from "../features/card/cardSlice";
 
 
 export const store = configureStore(
@@ -8,6 +9,7 @@ export const store = configureStore(
         reducer: {
             accounts: accountReducers,
             pages: pageReducers,
+            card: cardReducers
         },
     }
 )
