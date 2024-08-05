4c0509d1673902daf51133b5e5eac69c77314dd0
iocods <Iocodes2000@gmail.com>
2024-08-05 02:47:33 +0100

    Created Api Layer

---
+++ src/api/api.js
@@ -0,0 +1,24 @@
+import axios from "axios";
+
+const axiosParam = {
+  baseURL: "http://localhost:8080",
+  timeout: 5000,
+  headers: {
+    'Content-Type': 'application/json',
+    'Authorization': `${localStorage.getItem('token')}`
+  }
+}
+
+const axiosInstance = axios.create(axiosParam);
+
+const api = (instance) => {
+  return {
+    get: (url) => instance.get(url),
+    post: (url, data) => instance.post(url, data),
+    put: (url, data) => instance.put(url, data),
+    delete: (url) => instance.delete(url),
+    patch: (url, data) => instance.patch(url, data)
+  }
+}
+
+export default api(axiosInstance);
